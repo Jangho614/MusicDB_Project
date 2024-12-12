0a820b02314f5d49133e14ce6c5772479ae827a1
Jangho614 <gimjangho197@gmail.com>
2024-12-12 15:01:33 +0900

    DatabaseConnect

--- app/src/main/java/com/example/musicdb_project/MainActivity.java
+++ app/src/main/java/com/example/musicdb_project/MainActivity.java
@@ -1,24 +1,34 @@
 package com.example.musicdb_project;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import org.w3c.dom.Node;
+import org.w3c.dom.UserDataHandler;
 
 public class MainActivity extends AppCompatActivity {
+    private UserDataHandler userDataHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        userDataHandler = new UserDataHandler(this) {
+            @Override
+            public void handle(short operation, String key, Object data, Node src, Node dst) {
+
+            }
+        };
+
+        // 예제 데이터 삽입
+        long rowId = userDataHandler.insertUser("Alice", 23);
+
+        if (rowId != -1) {
+            Toast.makeText(this, "데이터 삽입 성공: Row ID " + rowId, Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "데이터 삽입 실패", Toast.LENGTH_SHORT).show();
+        }
     }
 }
